0eb6c60ab3cd8eb51cdfcabd0ba23d2cab102bbe
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-04-22 16:12:03 +0300

    Update scoped_ptr.cpp

--- tests/scoped_ptr.cpp
+++ tests/scoped_ptr.cpp
@@ -1,4 +1,42 @@
 #include <catch.hpp>
 #include <sstream>
-
 #include "scoped_ptr"
+
+TEST_CASE("Create ptr"){
+	scoped_ptr <int> My_ptr;
+	REQUIRE(My_ptr == nullptr);
+}
+
+TEST_CASE("Test ptr(n), op *, op get()"){
+	scoped_ptr <int> My_ptr1(new int(1));
+	int *ptr_1 = My_ptr1.get();
+	
+	scoped_ptr <int> My_ptr2(new int(2));
+	int ptr_2 = *My_ptr2;
+	
+	REQUIRE(*ptr_1 == 1);
+	REQUIRE(ptr_2 == 2);
+}
+
+TEST_CASE("Test swap"){
+	scoped_ptr <int> My_ptr1(new int(3));
+	
+	scoped_ptr <int> My_ptr2(new int(6));
+	
+	My_ptr1.swap(My_ptr2);
+	
+	int *ptr_1 = My_ptr1.get();
+	int ptr_2 = *My_ptr2;
+	
+	REQUIRE(*ptr_1 == 6);
+	REQUIRE(ptr_2 == 3);	
+}
+
+TEST_CASE("Test reset"){
+	scoped_ptr <int> My_ptr1(new int(3));
+	My_ptr1.reset(new int(6));
+	
+	int *ptr = My_ptr1.get();
+	
+	REQUIRE(*ptr == 6);
+}
